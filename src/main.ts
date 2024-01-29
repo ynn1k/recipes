62b97afd61366f16149fda90a852263056720127
Yannik Hewlik <hi@yannik.dev>
2024-01-29 20:45:39 +0100

    add swaggerUI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Use DocumentBuilder to create a new Swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle('Recipes API') // Set the title of the API
+    .setDescription('Recipes API description') // Set the description of the API
+    .setVersion('0.1') // Set the version of the API
+    .build(); // Build the document
+
+  // Create a Swagger document using the application instance and the document configuration
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup Swagger module with the application instance and the Swagger document
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
